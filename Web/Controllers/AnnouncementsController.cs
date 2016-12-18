3edda843c4d53f419c64f2819f64eb97a1b3effc
carlo <carlo@DESKTOP-3I05RQ0>
2016-12-18 12:03:55 +0000

    Add project files.

---
+++ Web/Controllers/AnnouncementsController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AnnouncementsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Announcements
+        public ActionResult Index()
+        {
+            string currentUserId = User.Identity.GetUserId();
+            ApplicationUser currentUser = db.Users.FirstOrDefault(
+                x => x.Id == currentUserId);
+            return View(db.Announcements.ToList().Where(x => x.User == currentUser));
+        }
+
+        // GET: Announcements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Announcements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,DateTime,Author,Content")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                //get user identity 
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault(
+                    x => x.Id == currentUserId);
+                announcement.User = currentUser;
+                //Setting the date automatically
+                announcement.DateTime = DateTime.Now;
+
+                db.Announcements.Add(announcement);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(announcement);
+        }
+
+        // GET: Announcements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(announcement);
+        }
+
+        // POST: Announcements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,DateTime,Author,Content")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(announcement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announcement announcement = db.Announcements.Find(id);
+            if (announcement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(announcement);
+        }
+
+        // POST: Announcements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Announcement announcement = db.Announcements.Find(id);
+            db.Announcements.Remove(announcement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult BuildAnnouncementTable()
+        {
+            return PartialView("_AnnouncementTable", db.Announcements.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AJAXCreate([Bind(Include = "Id,Title,Author,Content")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                //get user identity 
+                string currentUserId = User.Identity.GetUserId();
+                ApplicationUser currentUser = db.Users.FirstOrDefault(
+                    x => x.Id == currentUserId);
+                announcement.User = currentUser;
+                //Setting the date automatically
+                announcement.DateTime = DateTime.Now;
+
+                db.Announcements.Add(announcement);
+                db.SaveChanges();
+
+            }
+
+            return PartialView("_AnnouncementsTable", db.Announcements.ToList());
+        }
+
+        //populate announcements with comments
+        public AnnouncementView populateAnnouncementViewModel(int id)
+        {
+            AnnouncementView AV = new AnnouncementView();
+            int currentAnnouncementId = id;
+            AV.Announcement = db.Announcements.FirstOrDefault(x => x.Id == id);
+            AV.Comments = db.Comments.Where(x => x.AnnouncementId == currentAnnouncementId).ToList();
+            return AV;
+        }
+    }
+}
---
+++ Web/Models/AnnouncementView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class AnnouncementView
+    {
+        public Announcement Announcement;
+        public List<Comment> Comments;
+    }
+}
